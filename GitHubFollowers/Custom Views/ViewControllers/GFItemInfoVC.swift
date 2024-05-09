b046c2773afead7f254c547ce43da2572bad1d30
Taha ÖZMEN <tahaozmn35@gmail.com>
2024-05-09 11:19:12 +0300

    ItemInfoVC - Superclass

---
+++ GitHubFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,65 @@
+//
+//  GFItemInfoVC.swift
+//  GitHubFollowers
+//
+//  Created by Taha Özmen on 9.05.2024.
+//
+
+import UIKit
+import SnapKit
+
+class GFItemInfoVC: UIViewController {
+    
+    let stackView       = UIStackView()
+    let itemInfoViewOne = GFItemInfoView()
+    let itemInfoViewTwo = GFItemInfoView()
+    let actionButton    = GFButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        layoutUI()
+        configureStackView()
+    }
+    
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor    = .secondarySystemBackground
+    }
+    
+    
+    private func configureStackView() {
+        stackView.axis          = .horizontal
+        stackView.distribution  = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+    
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(padding)
+            make.leading.equalToSuperview().offset(padding)
+            make.trailing.equalToSuperview().offset(-padding)
+            make.height.equalTo(50)
+        }
+        
+        actionButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-padding)
+            make.leading.equalToSuperview().offset(padding)
+            make.trailing.equalToSuperview().offset(-padding)
+            make.height.equalTo(44)
+        }
+    }
+    
+
+    
+
+}
